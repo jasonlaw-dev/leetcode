70289df924ff36301d7dcf7747d13aec3533217b
Jason Law <jasonlaw.dev@gmail.com>
2019-12-17 16:44:55 +0800

    progress

---
+++ src/104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=104 lang=java
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+}
+// @lc code=end
+
