92d6a2c4471d0742d73e97ad9dd585b528f14672
Jason Law <jasonlaw.dev@gmail.com>
2020-06-27 13:10:50 +0800

    progress

--- src/668.kth-smallest-number-in-multiplication-table(revision-0).java
+++ src/668.kth-smallest-number-in-multiplication-table(revision-0).java
@@ -12,50 +12,30 @@ import java.util.PriorityQueue;
  * [2, 2, 3]
  * [3, 2, 3]
  * 
-1	2
-2	4
-3	6
-4   8
-5   10
-
-int[2] [rowIndex, colIndex]
-
-if we want to find 6th number
-
-to get the ith square, we calc it by Math.ceiling(sqar(k))
-
-if (the square can fit the table ) {
-    index of the number = k - (i-1)^2
-    i * ((index + 1) / 2)
-} else {
-
-}
-
-int i = (int) (Math.ceil(Math.sqrt(k)));
-        if (i <= m && i <= n) {
-            int idx = k - (i - 1) * (i - 1);
-            return i * ((idx + 1) / 2);
-        }
 */
 
 // @lc code=start
 class Solution {
     public int findKthNumber(int m, int n, int k) {
-        int[] nums = new int[m * n];
-        int min = 0;
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i + 1) * (j + 1) <= min) {
-                    count++;
-                } else {
-                    min = (i + 1) * (j + 1);
-                }
-                nums[i * n + j] = (i + 1) * (j + 1);
+        int low = 1;
+        int high = m * n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = count(m, n, mid);
+            if (count < k) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
-        Arrays.sort(nums);
-        return nums[k - 1];
+        return low;
+    }
+    private int count(int m, int n, int target) {
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            count += Math.min(target / i, n);
+        }
+        return count;
     }
 }
 // @lc code=end
